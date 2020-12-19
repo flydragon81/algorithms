94e943853421edb791785f910b10580a84a99f8b
flydragon81 <lyong1981@163.com>
2020-12-19 13:05:17 +0000

    average

--- functions.py
+++ functions.py
@@ -35,3 +35,18 @@ def find_biggest_number(data):
         i += 1
 
     return idx
+
+
+def average_number(data):
+    """
+
+    :param data: list
+    :return: float
+    """
+    total = 0
+    i = 0
+    while i < len(data):
+        total += data[i]
+        i += 1
+    avg = total / i
+    return avg
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,5 @@
 import unittest
-from functions import find_smallest_number
-from functions import find_biggest_number
+from functions import find_smallest_number, find_biggest_number, average_number
 
 
 class FindSmallestNumberTest(unittest.TestCase):
@@ -25,3 +24,10 @@ class FindBiggestNumberTest(unittest.TestCase):
         idx = find_biggest_number(data)
         self.assertEqual(5, idx)
         self.assertEqual(155, data[idx])
+
+
+class AverageNumberTest(unittest.TestCase):
+    def test(self):
+        data = [6, 8, 10, 17, 14]
+        avg = average_number(data)
+        self.assertEqual(11, avg)
